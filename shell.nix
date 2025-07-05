c46c5347a3baa345291ebec770c0c6de957b0388
CreggEgg <eggcregg@gmail.com>
2025-07-04 22:16:56 -0400

    initial commit

---
+++ shell.nix
@@ -0,0 +1,20 @@
+{ pkgs ? import <nixpkgs> { } }:
+
+with pkgs;
+
+mkShell rec {
+  nativeBuildInputs = [
+    pkg-config
+  ];
+  buildInputs = [
+    udev alsa-lib-with-plugins vulkan-loader
+    xorg.libX11 xorg.libXcursor xorg.libXi xorg.libXrandr # To use the x11 feature
+    libxkbcommon wayland # To use the wayland feature
+    clang
+    mold
+  ];
+  shellHook = ''
+  export LD_LIBRARY_PATH="${lib.makeLibraryPath buildInputs}";
+  '';
+  LD_LIBRARY_PATH=lib.makeLibraryPath buildInputs;
+}
